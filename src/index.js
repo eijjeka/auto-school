1c2447bd4856d3513e894f392b1274c6efc8f1bd
Saniokkk <kashirin.alexsandr91@gmail.com>
2023-09-25 20:18:20 +0300

    add send mail

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import './js/multi-modal.js';
+import './js/form.js';
 import "./js/header-bg.js";
 import "./js/scroll-top.js";
 import './script/openContact';
