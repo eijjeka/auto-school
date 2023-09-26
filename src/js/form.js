a3ed144b325fca39b6704c09d3145caa87560cdf
Saniokkk <kashirin.alexsandr91@gmail.com>
2023-09-26 15:31:53 +0300

    change alert

--- src/js/form.js
+++ src/js/form.js
@@ -23,7 +23,7 @@ async function formSend(e) {
   });
   if (response.ok) {
     let result = await response.json();
-    // alert(result.message);
+    alert(result.message);
     form.reset();
   }
 
